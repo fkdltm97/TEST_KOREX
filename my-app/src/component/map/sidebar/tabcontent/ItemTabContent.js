310758363cf6a886df323b560da6f324c40a8da8
DESKTOP-8ITRTI1\ITCODY <gotodongyang@naver.com>
2021-05-04 21:01:49 +0900

    충돌수정 병합 커밋진행>>

--- my-app/src/component/map/sidebar/tabcontent/ItemTabContent.js
+++ my-app/src/component/map/sidebar/tabcontent/ItemTabContent.js
@@ -27,6 +27,7 @@ export default function ItemTabContent({updatePageIndex, setHistoryInfo,setRepor
     // console.log(value.item_id);
 
     updatePageIndex(1,value.item_id);
+    //updatePageIndex(1,17);
     setHistoryInfo(e => {e.prevIndex.push(index);
     return JSON.parse(JSON.stringify(e));});
   }
